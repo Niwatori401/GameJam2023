91bba1b1de09a5a4d4954f55c1fc0aaa3e46cbbc
Niwatori401 <->
2023-08-26 17:37:47 -0700

    Add missing files for Antarctica, fixing crash

--- src/conf.lua
+++ src/conf.lua
@@ -5,7 +5,7 @@ function love.conf(t)
     t.version = "11.4"
     t.window.width = data.window.SCREEN_X
     t.window.height = data.window.SCREEN_Y
-    t.console = true
+    t.console = false
     t.window.borderless = false
     t.window.fullscreen = false         -- Enable fullscreen (boolean)
     t.window.fullscreentype = "exclusive" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
